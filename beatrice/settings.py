659bb0c4bca509fb15f570a3c9f7541729625592
Rafał Różak <rafalrozak@gmail.com>
2015-04-17 09:18:13 +0200

    fix configs

--- beatrice/settings.py
+++ beatrice/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'beatrice.wsgi.application'
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config() # {
+DATABASES = { 'default': dj_database_url.config() } # {
     # 'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
