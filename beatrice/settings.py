e643c8114c530e6c85b7c63c74eba1b486f0bff2
Rafał Różak <rafalrozak@gmail.com>
2015-05-14 00:30:43 +0200

    fix typo

--- beatrice/settings.py
+++ beatrice/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'beatrice.wsgi.application'
 
 import dj_database_url
 
-DATABASES['default']: dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
