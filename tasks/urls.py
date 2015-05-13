e46fd6a705fa62904b40efde85ebf2f9f30c3136
Rafał Różak <rafalrozak@gmail.com>
2015-05-14 00:25:13 +0200

    add category & notification endpoints

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,8 @@ from tasks.views import *
 urlpatterns = [
     url(r'^$', TaskList.as_view(), name='task_list'),
     url(r'^(?P<id>\d+)/$', TaskDetail.as_view(), name='task_detail'),
+    url(r'^categories/$', CategoryList.as_view(), name='task_list'),
+    url(r'^categories/(?P<id>\d+)/$', CategoryDetail.as_view(), name='task_detail'),
+    url(r'^notifications/$', NotificationList.as_view(), name='task_list'),
+    url(r'^notifications/(?P<id>\d+)/$', NotificationDetail.as_view(), name='task_detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework.generics import (
     ListCreateAPIView, RetrieveUpdateDestroyAPIView )
 from tasks.models import Task, Category, Notification
-from tasks.serializers import TaskSerializer
+from tasks.serializers import (
+    TaskSerializer, CategorySerializer, NotificationSerializer)
+
 
 class TaskMixin(object):
     queryset = Task.objects.all()
@@ -14,4 +16,30 @@ class TaskList(TaskMixin, ListCreateAPIView):
 
 
 class TaskDetail(TaskMixin, RetrieveUpdateDestroyAPIView):
+    pass
+
+
+class CategoryMixin(object):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryList(CategoryMixin, ListCreateAPIView):
+    pass
+
+
+class CategoryDetail(CategoryMixin, RetrieveUpdateDestroyAPIView):
+    pass
+
+
+class NotificationMixin(object):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+
+class NotificationList(NotificationMixin, ListCreateAPIView):
+    pass
+
+
+class NotificationDetail(NotificationMixin, RetrieveUpdateDestroyAPIView):
     pass
