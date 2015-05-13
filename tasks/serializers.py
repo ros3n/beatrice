5700bd3848800aa28a566fcccc1297b19a956fae
Rafał Różak <rafalrozak@gmail.com>
2015-05-13 23:57:01 +0200

    add tasks serializers

---
+++ tasks/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from rest_framework.serializers import ModelSerializer
+from tasks.models import Category, Task, Notification
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name',)
+
+
+class TaskSerializer(ModelSerializer):
+    class Meta:
+        model = Task
+        fields = (
+            'name', 'description', 'status', 'point', 'radius',
+            'category', 'notifications',
+        )
+
+
+class NotificationSerializer(ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = ('date', 'task')
